b93f2ad0b723852a29973a5961b712995ce647df
c4zxs56vtw-hub <onobionologo69@icloud.com>
2026-09-24 15:20:59 +0100

    first commit

---
+++ src/main/java/com/stockpilot/StockpilotApiApplication.java
@@ -0,0 +1,13 @@
+package com.stockpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockpilotApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockpilotApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockpilot/StockpilotApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockpilotApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
